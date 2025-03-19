7321d3066f42bb13fc6c0418b4a79310cbb76507
Ng Chuan Chieh <ncchieh@gmail.com>
2025-03-19 20:30:05 +0800

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -58,13 +58,13 @@ function calculate() {
 
     switch (operator) {
       case "+":
-        result = num1 + num2;
+        result = num2 + num1;
         break;
       case "-":
         result = num2 - num1;
         break;
       case "*":
-        result = num1 * num2;
+        result = num2 * num1;
         break;
       case "/":
         if (num2 === 0) {
